057e33984a2357287fcd9b82dae94bcbdfb1635a
johku <johannes.hakkarainen@helsinki.fi>
2021-08-23 12:15:30 +0300

    Fixed lint tests

--- src/discordBot/commands/faculty/poll.js
+++ src/discordBot/commands/faculty/poll.js
@@ -1,83 +1,82 @@
-const { MessageEmbed } = require('discord.js');
+/* eslint-disable no-var */
+const { MessageEmbed } = require("discord.js");
 const { sendEphemeral } = require("../utils");
 
-const execute = async (interaction, client, Groups) => {
-    
-    // return sendEphemeral(client, interaction, `Invalid course name: ${courseName} or the course is private already.`);
+const execute = async (interaction, client) => {
 
-    // const channel = client.channels.cache.find(c => c.name === "general" && c.type === "text");
+  const guild = client.guild;
+  const channel = guild.channels.cache.get(interaction.channel_id);
 
-    const guild = client.guild;
-    const channel = guild.channels.cache.get(interaction.channel_id);
+  const pollTitle = interaction.data.options[0].value.trim();
 
-    // const guild = client.guild;
-    // const channel = guild.channels.cache.get(interaction.channel_id).parent;
+  // If multiple choice poll
+  if (interaction.data.options[1]) {
 
-    const pollTitle = interaction.data.options[0].value.trim();
+    const answers = interaction.data.options[1].value.split("," || ", ");
+    const numbers = [ "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟", "🟥", "🟧", "🟩", "🟦", "🟫"];
 
-    // If multiple choice poll
-    if (interaction.data.options[1]) {
-
-        const answers = interaction.data.options[1].value.split(',' || ', ');
-        const numbers = [ '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟', '🟥', '🟧', '🟩', '🟦', '🟫'];
-
-        let answerOptions = '';
+    let answerOptions = "";
 
 
-        // Inizialize answer options text
-        for (var i = 0, len = answers.length; i < len; i++) {
-            answerOptions = answerOptions.concat(numbers[i] + ' = ' + answers[i] + '\n\n');
-        }
-
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(pollTitle)
-            .setDescription(answerOptions);
-
-        let msgEmbed = await channel.send(exampleEmbed);
-
-        for (var i = 0, len = answers.length; i < len; i++) {
-            msgEmbed.react(numbers[i]);
-        }
-
-        sendEphemeral(client, interaction, `Poll ready`);
-
-    //If yes/no poll
-    } else {
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(pollTitle)
-
-        let msgEmbed = await channel.send(exampleEmbed);
-        
-        await msgEmbed.react('👍')
-        await msgEmbed.react('👎')
-
-        sendEphemeral(client, interaction, `Poll ready`);
+    // Inizialize answer options text
+    for (var i = 0, len = answers.length; i < len; i++) {
+      answerOptions = answerOptions.concat(numbers[i] + " = " + answers[i] + "\n\n");
     }
+
+    const exampleEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle(pollTitle)
+      .setDescription(answerOptions);
+
+    // eslint-disable-next-line prefer-const
+    let msgEmbed = await channel.send(exampleEmbed);
+
+    // eslint-disable-next-line no-redeclare
+    for (var i = 0, len = answers.length; i < len; i++) {
+      msgEmbed.react(numbers[i]);
+
+    }
+
+    sendEphemeral(client, interaction, "Poll ready");
+
+    // If yes/no poll
+  }
+  else {
+    const exampleEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle(pollTitle);
+
+    // eslint-disable-next-line prefer-const
+    let msgEmbed = await channel.send(exampleEmbed);
+
+    await msgEmbed.react("👍");
+    await msgEmbed.react("👎");
+
+    sendEphemeral(client, interaction, "Poll ready");
+  }
 };
 
 module.exports = {
-    name: "poll",
-    description: "Create a poll",
-    usage: "[course name]",
-    args: true,
-    joinArgs: true,
-    guide: true,
-    role: "teacher",
-    options: [
-        {
-            name: "title",
-            description: "Set the title of the poll",
-            type: 3,
-            required: true,
-        },
-        {
-            name: "answers",
-            description: "Possible answers",
-            type: 3,
-            required: false,
-        },
-    ],
-    execute,
+  name: "poll",
+  description: "Create a poll",
+  usage: "[course name]",
+  args: true,
+  joinArgs: true,
+  guide: true,
+  role: "teacher",
+  options: [
+    {
+      name: "title",
+      description: "Set the title of the poll",
+      type: 3,
+      required: true,
+    },
+    {
+      name: "answers",
+      description: "Possible answers",
+      type: 3,
+      required: false,
+    },
+  ],
+  execute,
 };
